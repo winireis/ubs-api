09a780c63f6d13d5c5c376ab28ab20b54bb718f4
Winicius Reis <winireis@gmail.com>
2018-10-05 12:32:07 -0300

    Geokit added to Clinic model

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
+# Geokit
+gem 'geokit-rails'
+ 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,5 +1,10 @@
 class Clinic < ApplicationRecord
   #Geokit
+  acts_as_mappable :default_units => :kms,
+                   :default_formula => :sphere,
+                   :distance_field_name => :distance,
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
   #Validations
   validates_presence_of :name, :address, :city, :phone, :latitude, :longitude,
                         :size, :adaptation_for_seniors, :medical_equipment,
