aaeb5228d5985623f602f54523e87353d8bf60dd
Winicius Reis <winireis@gmail.com>
2018-10-05 12:28:10 -0300

    Seed Clinics database with raw data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+score = {
+	"Desempenho mediano ou  um pouco abaixo da média" => 1,
+	"Desempenho acima da média" => 2,
+	"Desempenho muito acima da média" => 3
+}
+counter = 0
+CSV.foreach(File.join(Rails.root, "db", "ubs.csv"), :headers => true, :encoding => 'UTF-8') do |row|
+	name = row['nom_estab'].to_s
+	address = row['dsc_endereco'].to_s
+	city = row['dsc_cidade'].to_s
+	phone = row['dsc_telefone'].to_s
+	latitude = row['vlr_latitude'].to_f
+	longitude = row['vlr_longitude'].to_f
+	size = score[row['dsc_estrut_fisic_ambiencia'].to_s]
+	adaptation_for_seniors = score[row['dsc_adap_defic_fisic_idosos'].to_s]
+	medical_equipment = score[row['dsc_equipamentos'].to_s]
+	medicine = score[row['dsc_medicamentos'].to_s]
+	Clinic.create!(
+		name: name,
+		address: address,
+		city: city,
+		phone: phone,
+		latitude: latitude,
+		longitude: longitude,
+		size: size,
+		adaptation_for_seniors: adaptation_for_seniors,
+		medical_equipment: medical_equipment,
+		medicine: medicine
+	)
+  counter += 1
+  puts "#{counter}"
+end
