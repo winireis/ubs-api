800917219be88481597332496495d21fe628760b
Winicius Reis <winireis@gmail.com>
2018-10-05 12:12:06 -0300

    Clinic model with passing tests

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,2 +1,7 @@
 class Clinic < ApplicationRecord
+  #Geokit
+  #Validations
+  validates_presence_of :name, :address, :city, :phone, :latitude, :longitude,
+                        :size, :adaptation_for_seniors, :medical_equipment,
+                        :medicine
 end
